766873373431915ed1655df2c97ec708ee78fe55
JaeYun <sim_518@naver.com>
2025-08-07 21:07:15 +0900

    feat: tts서비스 약간 수정 tts_service.dart : 초기 설정 함수 타입 변경, setVoice(음성 설정) 함수 추가

--- lib/services/tts_service.dart
+++ lib/services/tts_service.dart
@@ -1,55 +1,45 @@
 import 'package:flutter_tts/flutter_tts.dart';
 
 class TtsService {
+  // tts 인스턴스 생성
   final FlutterTts _flutterTts = FlutterTts();
 
-  TtsService() {
-    _initTts(); //초기 설정
-  }
+  TtsService();
 
-  // 초기 설정 함수
-  void _initTts() async {
-    await _flutterTts.setLanguage("ko-KR"); // 한국어
-    await _flutterTts.setVoice({"name": "ko-kr-x-ism-local", "locale": "ko-KR"}); // 한국어 여성 음성
+  // 기본 설정으로 초기화 (앱 시작 시 한 번 호출하기)
+  Future<void> init() async {
+    await _flutterTts.setLanguage("ko-KR"); // 한국어로 설정
+    await _flutterTts.setVoice({
+      "name": "ko-kr-x-ism-local",
+      "locale": "ko-KR"
+    }); // 한국어 여성 음성
     await _flutterTts.setSpeechRate(0.5);   // 말하는 속도 (0.0 ~ 1.0)
-    await _flutterTts.setPitch(1.0);        // 목소리 톤 (0.5 ~ 2.0)
+    await _flutterTts.setPitch(1.0);        // 목소리 톤(음높이) (0.5 ~ 2.0)
     await _flutterTts.setVolume(1.0);       // 볼륨 (0.0 ~ 1.0)
   }
 
-  /* 언어 설정 setLanguage
-  한국어 = "ko-KR"
-  일본어 = "ja-JP"
-  영어 = "en-US"
-  중국어 = "zh-CN"
-  */
 
-  /* 음성 설정 setVoice
-	영어 여성 {"name": "en-us-x-tpf-local", "locale": "en-US"}
-  일본어 여성 {"name": "ja-JP-language", "locale": "ja-JP"}
-  중국어 여성 {"name": "cmn-cn-x-ccc-local", "locale": "zh-CN"}
-  중국어 남성 {"name": "cmn-cn-x-ccd-local", "locale": "zh-CN"}
-  */
-
-  // TTS iOS 옵션
+  /*IOS 전용 초기 설정*/
   Future<void> initTtsIosOnly() async {
     // iOS 전용 옵션 : 공유 오디오 인스턴스 설정
     await _flutterTts.setSharedInstance(true);
 
     // 배경 음악와 인앱 오디오 세션을 동시에 사용
     await _flutterTts.setIosAudioCategory(
-        IosTextToSpeechAudioCategory.ambient,
-        [
-          IosTextToSpeechAudioCategoryOptions.allowBluetooth,
-          IosTextToSpeechAudioCategoryOptions.allowBluetoothA2DP,
-          IosTextToSpeechAudioCategoryOptions.mixWithOthers
-        ],
-        IosTextToSpeechAudioMode.voicePrompt);
+      IosTextToSpeechAudioCategory.ambient,
+      [
+        IosTextToSpeechAudioCategoryOptions.allowBluetooth,
+        IosTextToSpeechAudioCategoryOptions.allowBluetoothA2DP,
+        IosTextToSpeechAudioCategoryOptions.mixWithOthers,
+      ],
+      IosTextToSpeechAudioMode.voicePrompt,
+    );
   }
 
-
-  // 텍스트 읽기 함수
+  // 텍스트 읽기
   Future<void> speak(String text) async {
     if (text.trim().isNotEmpty) {
+      await _flutterTts.stop(); // 기존 음성 중지
       await _flutterTts.speak(text);
     }
   }
@@ -59,17 +49,32 @@ class TtsService {
     await _flutterTts.stop();
   }
 
-  // 언어 설정 (예: "en-US", "ko-KR")
+  /* 음성 설정 setVoice
+	영어 여성 {"name": "en-us-x-tpf-local", "locale": "en-US"}
+  일본어 여성 {"name": "ja-JP-language", "locale": "ja-JP"}
+  중국어 여성 {"name": "cmn-cn-x-ccc-local", "locale": "zh-CN"}
+  중국어 남성 {"name": "cmn-cn-x-ccd-local", "locale": "zh-CN"}
+  */
+  Future<void> setVoice(Map<String, String> voice) async {
+    await _flutterTts.setVoice(voice);
+  }
+
+  /* 언어 설정 setLanguage
+  한국어 = "ko-KR"
+  일본어 = "ja-JP"
+  영어 = "en-US"
+  중국어 = "zh-CN"
+  */
   Future<void> setLanguage(String langCode) async {
     await _flutterTts.setLanguage(langCode);
   }
 
-  // 속도 설정 (0.0 ~ 1.0)
+  // 말하기 속도 설정 (0.0 ~ 1.0)
   Future<void> setSpeechRate(double rate) async {
     await _flutterTts.setSpeechRate(rate);
   }
 
-  // 음 높이 설정 (0.5 ~ 2.0)
+  // 음높이 설정 (0.5 ~ 2.0)
   Future<void> setPitch(double pitch) async {
     await _flutterTts.setPitch(pitch);
   }
@@ -79,5 +84,3 @@ class TtsService {
     await _flutterTts.setVolume(volume);
   }
 }
-
-
