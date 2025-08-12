5d1263b12466dd9326ff38f807abdc572ecee1fb
ksh <rlatlgus925@naver.com>
2025-08-12 17:20:34 +0900

    chore: main.dart복구

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'services/auth_provider.dart';
-import 'services/user_provider.dart';
-import 'routes/router.dart';
-
+import 'package:seein_frontend/screens/select_home_screen.dart';
 // screen참고 testhome =영수증, product_analysis가 상품분석. select home임의로 만들어놓음.
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final providerManager = AuthProvider();
   await dotenv.load(fileName: ".env");
-  await providerManager.loadToken();
-  print("Token Loading Success");
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider<AuthProvider>.value(value: providerManager),
-        ChangeNotifierProvider<UserProvider>.value(value: UserProvider()),
-      ],
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SeeIn',
       theme: ThemeData(
         useMaterial3: true,
-        primaryColor: Color(0xFF8685EF),
-        secondaryHeaderColor: Color(0xFFDEDCEE),
-        scaffoldBackgroundColor: Color(0xFFFAFAFA),
-        cardColor: Color(0xFFFFFFFF),
-        // font style set
-        fontFamily: 'Pretendard',
-        textTheme: TextTheme(
-          // 폰트 크기, 색상, 굵기 스타일 지정
-          displayMedium: TextStyle(
-            fontSize: 52,
-            color: Color(0xFF8685EF),
-            fontWeight: FontWeight.w700,
-          ),
-          // 큰 제목
-          headlineLarge: TextStyle(
-            fontSize: 48,
-            color: Color(0xFF8685EF),
-            fontWeight: FontWeight.w700,
-          ),
-          //  페이지 제목
-          titleLarge: TextStyle(
-            fontSize: 26,
-            color: Color(0xFF8685EF),
-            fontWeight: FontWeight.w700,
-          ),
-          // 앱바 제목, 카드 제목 등
-          titleMedium: TextStyle(
-            fontSize: 20,
-            color: Colors.black,
-            fontWeight: FontWeight.w500,
-          ),
-          // 본문 제목
-          bodyMedium: TextStyle(
-            fontSize: 18,
-            color: Colors.black,
-            fontWeight: FontWeight.w400,
-          ),
-          // 본문
-          bodySmall: TextStyle(
-            fontSize: 18,
-            color: Color(0xFF8685EF),
-            fontWeight: FontWeight.w400,
-          ),
-          // 설명, 부가 텍스트
-          labelLarge: TextStyle(
-            fontSize: 18,
-            color: Colors.white,
-            fontWeight: FontWeight.w700,
-          ),
-          // 버튼 등 UI 텍스트
-          labelSmall: TextStyle(
-            fontSize: 16,
-            color: Color(0xFF8685EF),
-            fontWeight: FontWeight.w400,
-          ), // 부가 텍스트
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.white,
         ),
-        // TextTheme Setting
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            backgroundColor: Color(0xFF8685EF),
-            shadowColor: Colors.transparent,
-            foregroundColor: Colors.black,
-            fixedSize: Size(300, 60),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
+            backgroundColor: Colors.blueAccent,
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontWeight: FontWeight.bold),
           ),
         ),
-        // ElevatedButtonTheme Setting
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xFF8685EF),
-          foregroundColor: Color(0xFFDEDCEE),
-        ), // AppBarTheme Setting
       ),
-      initialRoute: RoutePage.start,
-      routes: RoutePage.appRoutes,
+      home: const SelectHomeScreen(),
     );
   }
 }
