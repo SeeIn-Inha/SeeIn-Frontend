f160074610317adf88c2e251ef6936e4f4b03bc6
ksh <rlatlgus925@naver.com>
2025-08-15 08:45:50 +0900

    feat: 비밀번호 찾기

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:seein_frontend/screens/users/edit_profile_screen.dart';
+import 'package:seein_frontend/screens/users/reset_password_screen.dart';
 import '../screens/start_screen.dart';
 import '../screens/users/join_screen.dart';
 import '../screens/users/login_screen.dart';
 import '../screens/camera_auto_analysis_screen.dart';
 import '../screens/select_home_screen.dart';
-import '../screens/users/my_info.dart';
+import '../screens/users/my_info_screen.dart';
 import '../screens/home_screen.dart';
 import '../screens/test_home_screen.dart';
 import '../screens/users/edit_profile_screen.dart';
@@ -22,6 +22,7 @@ class RoutePage {
   static const String realHome = '/realHome';
   static const String receipt = '/receipt';
   static const String editProfile = '/eiditProfile';
+  static const String resetPassword = '/resetPassword';
 
   // screens/...
   static final Map<String, WidgetBuilder> appRoutes = {
@@ -34,5 +35,6 @@ class RoutePage {
     realHome: (context) => Home(),
     receipt: (context) => HomeScreen(),
     editProfile: (context) => EditProfileScreen(),
+    resetPassword: (context) => ResetPassword(),
   };
 }
